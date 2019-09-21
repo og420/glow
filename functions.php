716f06675982ccd60eff99c23ddb251ef4bd7179
kodera123 <b.b.015.k.k@gmail.com>
2019-09-21 23:02:55 +0900

    多分 テンプレートっぽいもの追加

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Twenty Nineteen functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */ ?>
