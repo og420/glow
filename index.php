f4510c8be95860e29199380809105950cb044407
kodera123 <b.b.015.k.k@gmail.com>
2019-10-01 06:16:11 +0900

    【WIP】top page (#3)
    * html の基礎記述
    
    * 基礎部分のCSS と HTML
    
    * height 微修正

--- index.php
+++ index.php
@@ -16,7 +16,59 @@
 
 get_header();
 ?>
-
-
+<header id="glow_header">
+  <div class='header_logo'>
+    <img src='http://placehold.jp/150x50.png' alt=''>
+  </div>
+  <div class='header_search_box'>
+    <input type='text'>
+    <span>🔍</span>
+  </div>
+  <div class='header_burger_menu'>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</header>
+<section id='main'>
+  <div class='mv_slide'></div>
+  <div class='mv_posts_container'>
+    <div class='mv_post_box dummy_box'></div>
+    <div class='mv_post_box dummy_box'></div>
+    <div class='mv_post_box dummy_box'></div>
+    <div class='mv_post_box dummy_box'></div>
+  </div>
+</section>
+<section id='news' clas='clearfix'>
+  <div class='news_left_container'>
+    <h2 class='news_heading'>NEWS</h2>
+    <div class='news_subtext'>
+      <p>2020年大麻合法化。</p>
+      <p>今世界で起きているグリーンラッシュと</p>
+      <p>日本の未来の話。</p>
+    </div>
+    <div class='news_description'>
+      <p></p>
+    </div>
+    <div class='news_read_more'>READ MORE</div>
+  </div>
+  <div class='news_right_container'>
+    <img src='http://placehold.jp/600x600.png' alt=''>
+  </div>
+</section>
+<section id='news_posts'>
+  <h2 class='news_posts_heading'>OTHER POSTS</h2>
+  <div class='news_posts_container'>
+    <div class='news_post_box dummy_box'></div>
+    <div class='news_post_box dummy_box'></div>
+    <div class='news_post_box dummy_box'></div>
+  </div>
+  <div class='posts_read_more'>「栽培の記事をもっと見る」</div>
+</section>
+<section id='medical'></section>
+<section id='medical_post'></section>
+<section id='about_us'></section>
 <?php
 get_footer();
