2a403492d62fd2c71328cabdf81d1ec027c1b0b0
Costigan <sonicn.rsk256@gmail.com>
2019-10-09 14:14:21 +0900

    statement途中まで(PC版)

--- statement.php
+++ statement.php
@@ -8,7 +8,7 @@ Template Name: statement
 <div id="fullpage">
 <div class="section">
   <section class="stmt stmt--slide_1">
-    <div class="stmt__text stmt__text--layout_A wow fadeIn" data-wow-delay="1s">
+    <p class="stmt__text stmt__text--layout_A wow fadeIn" data-wow-delay="1s">
       <span class="stmt__text--type_A">
         誤解を恐れずに問いたい…<br>
         この国に住む私たちは、今本当は不幸なんじゃないか？
@@ -22,8 +22,8 @@ Template Name: statement
       いつしか分かり合うことさえ諦めたパートナー同士。<br><br>
 
       私たちの価値はまるで、どれほど自分を押し殺せたかだけで決まっているかのようだ。
-    </div>
-    <div class="stmt__text stmt__text--layout_B wow fadeIn" data-wow-delay="4s">
+    </p>
+    <p class="stmt__text stmt__text--layout_B wow fadeIn" data-wow-delay="4s">
       <span class="stmt__text--type_A">
       「仕方ない…これが現実だ。」<br>
       自分に言い聞かせる。いつしか、おかしいとさえ感じなくなってしまった。
@@ -35,12 +35,12 @@ Template Name: statement
 
       週末には、何となく話題のグルメスポットへ行く。街を歩けば、いつも似たような服を買ってしまう。<br>
       「何か楽しいことないかな …。」 そんな風にしてまた休日が終わる。また次の月曜日が始まる。
-    </div>
+    </p>
   </section>
 </div>
 <div class="section">
   <section class="stmt stmt--slide_2">
-    <div class="stmt__text stmt__text--layout_C">
+    <p class="stmt__text stmt__text--layout_C">
       <span class="stmt__text--type_B wow fadeIn" data-wow-delay="1s">
       でも、こんなのおかしくないだろうか？<br><br>
       </span>
@@ -65,12 +65,12 @@ Template Name: statement
       「嫌われないように、一生懸命頑張らないと…<br>
       素の自分を認めてくれる人なんてどこにもいない。」<br>
       </span>
-    </div>
+    </p>
   </section>
 </div>
 <div class="section">
   <section class="stmt stmt--slide_3">
-    <div class="stmt__text stmt__text--layout_C">
+    <p class="stmt__text stmt__text--layout_C">
       <span class="stmt__text--type_B wow fadeIn" data-wow-delay="1s">
         それにしたって、私たちはどうしてこんなにも<br>
         自信を失ってしまっているのだろう？<br><br>
@@ -84,12 +84,12 @@ Template Name: statement
       <span class="stmt__text--type_B wow fadeIn stmt__text--deco_underline" data-wow-delay="3s">
         何も決められていないからじゃないだろうか？
       </span>
-    </div>
+    </p>
   </section>
 </div>
 <div class="section">
   <section class="stmt stmt--slide_4">
-    <div class="stmt__text stmt__text--layout_D">
+    <p class="stmt__text stmt__text--layout_D">
       <span class="stmt__text--type_A wow fadeIn" data-wow-delay="1s">
         ここで少しだけ、他国に目を向けてみよう。<br>
         例えば、デンマークやノルウェーといった北欧諸国。<br><br>
@@ -110,14 +110,110 @@ Template Name: statement
         2018年度、日本はそれぞれ国際的に低い54位と22位。<br>
         ただ、こんな調査を語るまでもないかもしれない。<br>
       </span>
-    </div>
-    <img src="<?php echo get_template_directory_uri();?>/imgs/happiness-ranking.png" alt="世界幸福度ランキング" style="margin-right:28px">
-    <img src="<?php echo get_template_directory_uri();?>/imgs/demo-ranking.png" alt="世界民主主義ランキング">
+    </p>
+    <img class="wow fadeIn" data-wow-delay="4s" src="<?php echo get_template_directory_uri();?>/imgs/happiness-ranking.png" alt="世界幸福度ランキング" style="margin-right:28px">
+    <img class="wow fadeIn" data-wow-delay="4.5s" src="<?php echo get_template_directory_uri();?>/imgs/demo-ranking.png" alt="世界民主主義ランキング">
   </section>
 </div>
 <div class="section">
   <section class="stmt stmt--slide_5">
-    <div class="stmt__text"></div>
+    <p class="stmt__text stmt__text--layout_C">
+      <span class="stmt__text--type_A wow fadeIn" data-wow-delay="1s">
+        自分の生活に直結しているはずの仕組みやルールが、<br>
+        自分の意も介さずに勝手に決まっていってしまう…。<br><br>
+      </span>
+      <span class="wow fadeIn" data-wow-delay="2s">
+        このどうしようもない無力感は、この国において<br>
+        誰もが当たり前に持っているもののように思える。<br><br>
+      </span>
+      <span class="stmt__text--type_B wow fadeIn stmt__text--deco_underline" data-wow-delay="3s">
+        そう、この国の民主主義が死んでいることは、</span><br>
+      <span class="stmt__text--type_B wow fadeIn stmt__text--deco_underline" data-wow-delay="3s">
+        私たちの無力感の根源のように思えないだろうか？</span><br><br>
+      <span class="wow fadeIn" data-wow-delay="4s">
+        確かに、私たちはこれまで声をあげてこなかった。<br>
+        選挙は愚か選ぶために知ることも、考えることも、<br>
+        全てを放棄してきた。いつもこうやって諦めてきた。<br>
+      </span>
+    </p>
+  </section>
+</div>
+<div class="section">
+  <section class="stmt stmt--slide_6">
+    <p class="stmt__text stmt__text--layout_C">
+      <span class="stmt__text--type_A wow fadeIn" data-wow-delay="1s">
+        「どうせ何も変わらない。変えられるはずがない。」
+      </span><br><br>
+      <span class="stmt__text--type_A stmt__text--deco_underline wow fadeIn" data-wow-delay="2s">
+        未来に希望さえ持てないこんな状態で、</span><br>
+      <span class="stmt__text--type_A stmt__text--deco_underline wow fadeIn" data-wow-delay="2s">
+        どうしたって本当に幸せだと感じられるだろう？</span>
+    </p>
+  </section>
+</div>
+<div class="section">
+  <section class="stmt stmt--slide_7">
+    <p class="stmt__text stmt__text--layout_C">
+      <span class="wow fadeIn" data-wow-delay="1s">
+        でも、ほんの少しだけでいいから想像してみよう。<br>
+        もし仮に、私たちの想いが実現されるとしたら？<br><br>
+      </span>
+      <span class="stmt__text--type_A stmt__text--deco_underline wow fadeIn" data-wow-delay="2s">
+        「本当はこんな国にしたい。こうあるべきだ。」
+      </span><br><br>
+      <span class="wow fadeIn" data-wow-delay="3s">
+        誰もが当たり前のように、この国の仕組みとルールを変えていける希望を持つことができたならば、<br>
+        もっと自信が湧いてくる気がしないだろうか？</span><br><br>
+      <span class="wow fadeIn" data-wow-delay="4s">
+        もう何にも縛られなくていい。本当にやりたいことを選ぶ勇気を、本当に一緒にいたい人を選ぶ勇気を、<br>
+        私たちはもう一度取り戻せるような気がする。</span><br>
+    </p>
+  </section>
+</div>
+<div class="section">
+  <section class="stmt stmt--slide_8">
+    <p class="stmt__text stmt__text--layout_C">
+      <span class="stmt__text--type_B wow fadeIn" data-wow-delay="2s">
+        だからこそ我々「GLOW」は、<br><br>
+      </span>
+      <span class="stmt__text--type_C stmt__text--deco_underline wow fadeIn" data-wow-delay="3s">
+        この国の⺠主主義を再機能させたい。
+      </span>
+    </p>
+  </section>
+</div>
+<div class="section">
+  <section class="stmt stmt--slide_9">
+    <p class="stmt__text stmt__text--layout_D"></p>
+  </section>
+</div>
+<div class="section">
+  <section class="stmt stmt--slide_10">
+    <p class="stmt__text stmt__text--layout_C">
+      <span class="stmt__text--type_E wow fadeIn" data-wow-delay="1s">
+        だから、ここから始めてみよう。<br>
+        賛成でも反対でも、どちらでもいい。</span><br><br>
+      <span class="stmt__text--type_E wow fadeIn" data-wow-delay="2s">
+        まずは私たち一人一人が意見を持つこと。<br>
+        きちんと声を上げていくこと。</span><br><br>
+      <span class="stmt__text--type_E wow fadeIn" data-wow-delay="3s">
+        やがてその声は大きなうねりとなり、<br>
+        この国の仕組みとルールを自分たちでも作っていける、<br>
+        そんな確かな希望に変わっていくだろう。</span><br><br>
+      <span class="stmt__text--type_E wow fadeIn" data-wow-delay="4s">
+        それが私たちの無力感からの解放、自信、幸福へ<br>
+        繋がっていく...我々は本気でそう信じている。</span><br><br>
+
+      <span class="stmt__text--type_F wow fadeIn" data-wow-delay="6s">
+        "重要なのは一人の偉大な指導者ではなく、名も無き人たちの無数の小さな活動の積み重ねである。<br>
+        そのような人たちが、歴史に残るような大きな出来事の土台を築いてきたのだ。"<br><br>
+
+        ハワード・ジン（アメリカ合衆国  歴史学者）</span><br><br>
+
+      <span class="stmt__text--type_B wow fadeIn stmt__text--deco_underline" data-wow-delay="8s">
+        そう、未来は私たちの手の中だ。</span>
+
+    </p>
   </section>
 </div>
 
