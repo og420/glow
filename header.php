c21e4ec34bc6edafa3df19bfd5bcd7628dc6f73f
Rendy Costigan <sonicn.rsk256@gmail.com>
2019-10-09 14:54:41 +0900

    CSS分割 (#6)

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/fullpage.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/statement.css">
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
 	<?php wp_head(); ?>
