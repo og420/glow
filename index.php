275ac93a29ab02a09c85887610e8898e5c8afd2e
kodera123 <b.b.015.k.k@gmail.com>
2019-10-11 00:57:37 +0900

    残りデザイン通りでないところの修正

--- index.php
+++ index.php
@@ -16,13 +16,15 @@
 
 get_header();
 ?>
-<header id="glow_header">
-  <div class='header_logo'>
-    <img src='http://placehold.jp/150x50.png' alt=''>
-  </div>
-  <div class='header_search_box'>
-    <input type='text'>
-    <span>🔍</span>
+<header id='glow_header'>
+  <div class='header_left_content'>
+    <div class='header_logo'>
+      <img src='http://placehold.jp/150x50.png' alt=''>
+    </div>
+    <div class='header_search_box'>
+      <input type='text'>
+      <span>🔍</span>
+    </div>
   </div>
   <div class='header_burger_menu'>
     <div></div>
@@ -34,9 +36,9 @@ get_header();
 </header>
 <section id='main'>
   <div class='mv_slide'>
-    <div>ホゲホゲ</div>
-    <div>ホゲホゲ</div>
-    <div>ホゲホゲ</div>
+    <div><p class='mv_slide_text'>ホゲホゲ</p></div>
+    <div><p class='mv_slide_text'>ホゲホゲ</p></div>
+    <div><p class='mv_slide_text'>ホゲホゲ</p></div>
   </div>
   <div class='mv_posts_container'>
     <div class='mv_post_box dummy_box'></div>
@@ -45,7 +47,7 @@ get_header();
     <div class='mv_post_box dummy_box'></div>
   </div>
 </section>
-<section id='news' clas='clearfix'>
+<section id='news' class='clearfix'>
   <div class='news-inner'>
     <div class='news_left_container'>
       <div class='news_heading'><img src='http://placehold.jp/300x100.png' alt=''></div>
@@ -66,17 +68,101 @@ get_header();
   </div>
 </section>
 <section id='news_posts'>
-  <h2 class='news_posts_heading'>OTHER POSTS</h2>
-  <div class='news_posts_container'>
-    <div class='news_post_box dummy_box'></div>
-    <div class='news_post_box dummy_box'></div>
-    <div class='news_post_box dummy_box'></div>
+  <div class='other_posts_inner'>
+    <h2 class='other_posts_heading'><img src='http://placehold.jp/300x100.png' alt=''></h2>
+    <div class='other_posts_container'>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+    </div>
+    <div class='other_posts_read_more'><div class='other_post_read_more_btn'>「栽培の記事をもっと見る」</div></div>
   </div>
-  <div class='posts_read_more'>「栽培の記事をもっと見る」</div>
 </section>
-<section id='medical'></section>
-<section id='medical_post'></section>
-<section id='about_us'></section>
+<section id='medical'>
+  <div class='medical-inner'>
+    <div class='medical_left_container'>
+      <img src='http://placehold.jp/700x600.png' alt=''>
+    </div>
+    <div class='medical_right_container'>
+      <div class='medical_heading'><img src='http://placehold.jp/300x100.png' alt=''></div>
+      <div class='medical_main_text'>
+        <p>2020年大麻合法化。</p>
+        <p>今世界で起きているグリーンラッシュと</p>
+        <p>日本の未来の話。</p>
+      </div>
+      <div class='medical_description'>
+        <p class='mb30'>そこはほか依然としてその意味痛によってのの以上にするですない。とにかく今日をお話家はもしそのままほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨ</p>
+        <p>そこはほか依然としてその意味痛によってのの以上にするですない。とにかく今日をお話家はもしそのままほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨほげほげポヨポヨ</p>
+      </div>
+      <div class='medical_read_more'><p>READ MORE</p></div>
+    </div>
+  </div>
+</section>
+<section id='medical_post'>
+  <div class='other_posts_inner'>
+    <h2 class='other_posts_heading'><img src='http://placehold.jp/300x100.png' alt=''></h2>
+    <div class='other_posts_container'>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+      <div class='other_post_box'>
+        <img src='http://placehold.jp/400x300.png' alt='' class='other_post_img'>
+        <div class='other_post_info_box'>
+          <div class='other_post_title mb10'>大麻によって税収アップ？アメリカ・カナダの事例を紹介</div>
+          <div class='other_post_datetime'>2019.01.01 04:20:08</div>
+        </div>
+      </div>
+    </div>
+    <div class='other_posts_read_more'><div class='other_post_read_more_btn'>「栽培の記事をもっと見る」</div></div>
+  </div>
+</section>
+<section id='about_us'>
+  <div class='about_us-inner'>
+    <h2 class='about_us_heading'><img src='http://placehold.jp/300x100.png' alt=''></h2>
+    <div class='about_us_main_container'>
+      <h3>大麻合法化の是非について考えることは、<br>あなた自身の幸福について考えることだ</h3>
+      <h5>誤解を恐れずに問いたい…この国に住む私たちは、今とても不幸なんじゃないか？</h5>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p class='mb50'>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <h4>だからこそ我々『GreenTimes』は<br>この国の民主主義を再機能させたい</h4>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <div class='about_us_bottom_content'>
+        <img src='http://placehold.jp/300x50.png' alt=''>
+        <div class='about_us_read_more'><p>READ MORE</p></div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <!-- slick script -->
 <script type="text/javascript">
