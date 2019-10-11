dc90ad8f076b047d2cc0b3f23bf9234c4cb24723
Costigan <sonicn.rsk256@gmail.com>
2019-10-11 13:28:26 +0900

    ヘッダー調整

--- index.php
+++ index.php
@@ -1,39 +1,6 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
 get_header();
 ?>
-<header id='glow_header'>
-  <div class='header_left_content'>
-    <div class='header_logo'>
-      <img src='http://placehold.jp/150x50.png' alt=''>
-    </div>
-    <div class='header_search_box'>
-      <input type='text'>
-      <span>🔍</span>
-    </div>
-  </div>
-  <div class='header_burger_menu'>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-</header>
 <section id='main'>
   <div class='mv_slide'>
     <div><p class='mv_slide_text'>ホゲホゲ</p></div>
