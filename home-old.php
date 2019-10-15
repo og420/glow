55eea4d07c2cb6185cbb5b9648353a024a25617b
Rendy Costigan <sonicn.rsk256@gmail.com>
2019-10-15 12:34:45 +0900

    brush-up (#18)

---
+++ home-old.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<section class="a__mv">
+  <p class="a__mv__welcomeMessage">
+    WELCOME TO <span>GLOW</span>
+  </p>
+</section>
+
+<section class="a__archive">
+  <div class="container">
+    <div class="a__articles">
+      <?php if(have_posts()): while(have_posts()): the_post(); ?>
+      	<article class="a__article">
+          <a href="<?php the_permalink(); ?>">
+            <div class="a__article__thumb">
+              <?php the_post_thumbnail('large'); ?>
+            </div>
+          </a>
+          <span class="a__article__category">
+            <?php
+              $postcat = get_the_category();
+              echo $postcat[0]->name;
+            ?>
+          </span>
+          <div class="a__article__label">
+            <h2 class="a__article__title">
+              <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?></a>
+            </h2>
+            <span class="a__article__date">
+              <time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
+                <?php echo get_the_date(); ?>
+              </time>
+            </span>
+          </div>
+        </article>
+      <?php endwhile; endif; ?>
+    </div>
+  </div>
+</section>
+
+<?php
+if( function_exists("the_pagination") ) the_pagination();
+?>
+
+<?php get_footer(); ?>
