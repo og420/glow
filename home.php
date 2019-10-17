dca12fc0aa429bfb7d60c1cbdbb2e41ef72b26e4
Costigan <sonicn.rsk256@gmail.com>
2019-10-17 16:45:05 +0900

    めっちゃコード修正 SP版実装 埒あかないからjustfixで後に大部分要書き換え

--- home.php
+++ home.php
@@ -54,7 +54,7 @@ get_header();
       foreach ( $posts as $post ): // ループの開始
       setup_postdata( $post ); // 記事データの取得
     ?>
-      <div style="background: url(<?php the_post_thumbnail_url('medium'); ?>);" class='mv_post_box'>
+      <div style="background: url(<?php the_post_thumbnail_url('large'); ?>);" class='mv_post_box'>
         <div class='mv_post_info'>
           <?php the_category(); ?>
           <p class="mv_post_title mb10">
@@ -62,13 +62,12 @@ get_header();
               <?php the_title(); ?>
             </a>
           </p>
-          <p class="mv_post_date"><?php the_date(); ?></p>
+          <p class="mv_post_date"><?php the_time('Y/m/d'); ?></p>
         </div>
       </div>
-    <?php
-      endforeach; // ループの終了
+      <?php endforeach; //ループの終了
       wp_reset_postdata(); // 直前のクエリを復元する
-    ?>
+      ?>
   </div>
 </section>
 <section id='news' class='clearfix'>
@@ -78,13 +77,13 @@ get_header();
                 'posts_per_page' => 1, // 表示する件数
                 'orderby' => 'date', // 日付でソート
                 'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
-                'category_name' => 'news' // 表示したいカテゴリーのスラッグを指定
+                'category_name' => 'politics-legal' // 表示したいカテゴリーのスラッグを指定
             );
       $posts = get_posts( $arg );
       if( $posts ): ?>
         <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
           <div class='news_left_container'>
-            <div class='news_heading'><span class="black_stroke">N</span>EWS</div>
+            <div class='news_heading'><span class="black_stroke">P</span>OLITICS</div>
             <div class='news_main_text'>
               <p><?php the_title(); ?></p>
             </div>
@@ -92,11 +91,11 @@ get_header();
               <p><?php the_excerpt(); ?></p>
             </div>
             <div class='news_read_more'>
-              <p>READ MORE</p>
+              <a href="<?php the_permalink(); ?>">READ MORE</p>
             </div>
           </div>
           <div class='news_right_container'>
-            <img src="<?php the_post_thumbnail_url('medium'); ?>" alt=''>
+            <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
           </div>
       <?php endforeach; ?>
     <?php endif;
@@ -115,23 +114,31 @@ get_header();
                   'offset'=> 1, // カテゴリ部分で1記事表示しているのでオフセットする
                   'orderby' => 'date', // 日付でソート
                   'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
-                  'category_name' => 'news' // 表示したいカテゴリーのスラッグを指定
+                  'category_name' => 'politics-legal' // 表示したいカテゴリーのスラッグを指定
               );
+        $cat = get_category_by_slug($arg['category_name']);
         $posts = get_posts( $arg );
         if( $posts ): ?>
         <?php
             foreach ( $posts as $post ) :
               setup_postdata( $post ); ?>
             <div class='other_post_box'>
-              <img src="<?php the_post_thumbnail_url('medium'); ?>" alt='' class='other_post_img'>
+              <div class="other_post_img">
+                <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
+              </div>
+              <span class="other_post_category">
+                <?php
+                  $postcat = get_the_category();
+                  echo $postcat[0]->name;
+                ?>
+              </span>
               <div class='other_post_info_box'>
-                <?php the_category(); ?>
                 <div class='other_post_title mb10'>
                   <a href="<?php the_permalink(); ?>">
                     <?php the_title(); ?>
                   </a>
                 </div>
-                <div class='other_post_datetime'><?php the_date(); ?></div>
+                <div class='other_post_datetime'><?php the_time('Y/m/d'); ?></div>
               </div>
             </div>
       <?php endforeach; ?>
@@ -141,7 +148,7 @@ get_header();
       ?>
     </div>
     <div class='other_posts_read_more'>
-      <div class='other_post_read_more_btn'>「栽培の記事をもっと見る」</div>
+    <a href="<?php echo get_category_link( $cat->cat_ID); ?>" class='other_post_read_more_btn'>「<?php echo get_cat_name($cat->cat_ID);?>」の記事をもっと見る</a>
     </div>
   </div>
 </section>
@@ -152,13 +159,13 @@ get_header();
                 'posts_per_page' => 1, // 表示する件数
                 'orderby' => 'date', // 日付でソート
                 'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
-                'category_name' => 'medical' // 表示したいカテゴリーのスラッグを指定
+                'category_name' => 'health-science' // 表示したいカテゴリーのスラッグを指定
             );
       $posts = get_posts( $arg );
       if( $posts ): ?>
         <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
           <div class='medical_left_container'>
-            <img src="<?php the_post_thumbnail_url('medium'); ?>" alt=''>
+            <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
           </div>
           <div class='medical_right_container'>
             <div class='medical_heading'><span class="black_stroke">M</span>EDICAL</div>
@@ -169,7 +176,7 @@ get_header();
               <p><?php the_excerpt(); ?></p>
             </div>
             <div class='medical_read_more'>
-              <p>READ MORE</p>
+              <a href="<?php the_permalink(); ?>">READ MORE</p>
             </div>
           </div>
       <?php endforeach; ?>
@@ -189,23 +196,31 @@ get_header();
                   'offset'=> 1, // カテゴリ部分で1記事表示しているのでオフセットする
                   'orderby' => 'date', // 日付でソート
                   'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
-                  'category_name' => 'medical' // 表示したいカテゴリーのスラッグを指定
+                  'category_name' => 'health-science' // 表示したいカテゴリーのスラッグを指定
               );
+        $cat = get_category_by_slug($arg['category_name']);
         $posts = get_posts( $arg );
         if( $posts ): ?>
         <?php
             foreach ( $posts as $post ) :
               setup_postdata( $post ); ?>
             <div class='other_post_box'>
-              <img src="<?php the_post_thumbnail_url('medium'); ?>" alt='' class='other_post_img'>
+              <div class="other_post_img">
+                <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
+              </div>
+              <span class="other_post_category">
+                <?php
+                  $postcat = get_the_category();
+                  echo $postcat[0]->name;
+                ?>
+              </span>
               <div class='other_post_info_box'>
-                <?php the_category(); ?>
                 <div class='other_post_title mb10'>
                   <a href="<?php the_permalink(); ?>">
                     <?php the_title(); ?>
                   </a>
                 </div>
-                <div class='other_post_datetime'><?php the_date(); ?></div>
+                <div class='other_post_datetime'><?php the_time('Y/m/d'); ?></div>
               </div>
             </div>
       <?php endforeach; ?>
@@ -215,7 +230,89 @@ get_header();
       ?>
     </div>
     <div class='other_posts_read_more'>
-      <div class='other_post_read_more_btn'>「栽培の記事をもっと見る」</div>
+    <a href="<?php echo get_category_link( $cat->cat_ID); ?>" class='other_post_read_more_btn'>「<?php echo get_cat_name($cat->cat_ID);?>」の記事をもっと見る</a>
+    </div>
+  </div>
+</section>
+<section id='news' class='clearfix'>
+  <div class='news-inner'>
+    <?php
+      $arg = array(
+                'posts_per_page' => 1, // 表示する件数
+                'orderby' => 'date', // 日付でソート
+                'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
+                'category_name' => 'business' // 表示したいカテゴリーのスラッグを指定
+            );
+      $posts = get_posts( $arg );
+      if( $posts ): ?>
+        <?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+          <div class='news_left_container'>
+            <div class='news_heading'><span class="black_stroke">B</span>USINESS</div>
+            <div class='news_main_text'>
+              <p><?php the_title(); ?></p>
+            </div>
+            <div class='news_description'>
+              <p><?php the_excerpt(); ?></p>
+            </div>
+            <div class='news_read_more'>
+              <a href="<?php the_permalink(); ?>">READ MORE</p>
+            </div>
+          </div>
+          <div class='news_right_container'>
+            <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
+          </div>
+      <?php endforeach; ?>
+    <?php endif;
+    wp_reset_postdata(); ?>
+  </div>
+</section>
+<section id='news_posts'>
+  <div class='other_posts_inner news_posts_inner'>
+    <h2 class='other_posts_heading'>
+      <span class="white_stroke">O</span>THER POSTS
+    </h2>
+    <div class='other_posts_container'>
+      <?php
+        $arg = array(
+                  'posts_per_page' => 4, // 表示する件数
+                  'offset'=> 1, // カテゴリ部分で1記事表示しているのでオフセットする
+                  'orderby' => 'date', // 日付でソート
+                  'order' => 'DESC', // DESCで最新から表示、ASCで最古から表示
+                  'category_name' => 'business' // 表示したいカテゴリーのスラッグを指定
+              );
+        $cat = get_category_by_slug($arg['category_name']);
+        $posts = get_posts( $arg );
+        if( $posts ): ?>
+        <?php
+            foreach ( $posts as $post ) :
+              setup_postdata( $post ); ?>
+            <div class='other_post_box'>
+              <div class="other_post_img">
+                <img src="<?php the_post_thumbnail_url('large'); ?>" alt=''>
+              </div>
+              <span class="other_post_category">
+                <?php
+                  $postcat = get_the_category();
+                  echo $postcat[0]->name;
+                ?>
+              </span>
+              <div class='other_post_info_box'>
+                <div class='other_post_title mb10'>
+                  <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                  </a>
+                </div>
+                <div class='other_post_datetime'><?php the_time('Y/m/d'); ?></div>
+              </div>
+            </div>
+      <?php endforeach; ?>
+      <?php
+        endif;
+        wp_reset_postdata();
+      ?>
+    </div>
+    <div class='other_posts_read_more'>
+      <a href="<?php echo get_category_link( $cat->cat_ID); ?>" class='other_post_read_more_btn'>「<?php echo get_cat_name($cat->cat_ID);?>」の記事をもっと見る</a>
     </div>
   </div>
 </section>
@@ -227,15 +324,21 @@ get_header();
     <div class='about_us_main_container'>
       <h3>大麻合法化の是非について考えることは、<br>あなた自身の幸福について考えることだ</h3>
       <h5 class="mb30">誤解を恐れずに問いたい…この国に住む私たちは、今とても不幸なんじゃないか？</h5>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-      <p class='mb70'>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>「仕方ない…これが現実だ。」自分に言い聞かせる。いつしか、おかしいとさえ感じなくなってしまった。</p>
+      <p class='mb70'>でも、ほんの少しだけでいいから想像してみよう。<br>
+        もし仮に、私たちの想いが実現されるとしたら？
+      </p>
       <h4 class='mb30'>だからこそ我々『GreenTimes』は<br>この国の民主主義を再機能させたい</h4>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-      <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p>
+        ここから始めてみよう。賛成でも反対でも、どちらでもいい。<br>まずは私たち一人一人が意見を持つこと。 きちんと声を上げていくこと。<br><br>
+        やがてその声は大きなうねりとなり、この国の仕組みとルールを自分たちでも作っていける、そんな確かな希望に変わっていくだろう。<br><br>
+        それが私たちの無力感からの解放、自信、幸福へ
+        繋がっていく…我々は本気でそう信じている。
+      </p>
       <div class='about_us_bottom_content'>
         <img src="<?php echo get_template_directory_uri(); ?>/imgs/logo.png" alt=''>
         <div class='about_us_read_more'>
-          <p>READ MORE</p>
+          <a href="<?php echo esc_url( home_url('/') ); ?>statement">READ MORE</a>
         </div>
       </div>
     </div>
