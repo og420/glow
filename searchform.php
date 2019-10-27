eccc731fc0f3648ce91526e01ddc30fbd6c7525d
Costigan <sonicn.rsk256@gmail.com>
2019-10-28 05:16:00 +0900

    中断

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="header__search" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+<form autocomplete="off" class="header__search" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
     <input class="header__searchInput" name="s" type="text" placeholder="検索したいキーワード ..."></input>
     <button type="submit" class="header__serachSubmit"><div class="header__searchSubmitText"><img src="<?php echo get_template_directory_uri(); ?>/imgs/search-icon.png"></div></button>
 </form>
